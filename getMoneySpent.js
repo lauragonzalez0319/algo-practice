eee4556d5d9dc0d0eef835407ec154e7bf492d92
Laura Gonzalez <lg458@cornell.edu>
2022-12-30 16:10:32 -0500

    Added get money spent hacker rank solution

---
+++ getMoneySpent.js
@@ -0,0 +1,23 @@
+//Hackerrank: https://www.hackerrank.com/challenges/electronics-shop/problem?isFullScreen=true
+
+function getMoneySpent(keyboards, drives, b) {
+  let maxSpent = 0;
+  
+  for (let i=0; i<keyboards.length; i++) {
+      for (let j=0; j<drives.length; j++) {
+          let cost = keyboards[i] + drives[j];
+          if (cost <= b) {
+              if (maxSpent === 0) {
+                  maxSpent = cost;
+              } else if (cost > maxSpent) {
+                  maxSpent = cost;
+              }
+          }
+      }
+  }
+  if (maxSpent === 0) {
+      return -1;
+  } else {
+      return maxSpent;
+  }
+}
